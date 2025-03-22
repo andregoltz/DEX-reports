d32a7bca05639f9c476ccae22f9154292b40a4a0
Andre Goltz <andregoltz@outlook.com.br>
2025-03-22 03:02:01 -0300

    Establishing communication between FrontEnd and BackEnd

---
+++ DEX.API/Extensions/BuilderExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace DEX.API.Extensions
+{
+    public static class BuilderExtension
+    {
+        public static void AddOpenApi(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddOpenApi();
+        }
+
+        public static void AddCors(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowBlazorApp",
+                    policy => policy.WithOrigins("https://localhost:7143") // Permitir Blazor
+                                    .AllowAnyMethod()
+                                    .AllowAnyHeader());
+            });
+        }
+    }
+}
---
+++ DEX.API/Extensions/DEXReportContextExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace DEX.API.Extensions
+{
+    public static class DEXReportContextExtension
+    {
+        public static void MapDEXEndpoints(this WebApplication app)
+        {
+            #region Post
+            app.MapPost("/api/v1/vdi-dex", async (HttpRequest request) =>
+            {
+                using var reader = new StreamReader(request.Body, Encoding.UTF8);
+                var dexContent = await reader.ReadToEndAsync();
+
+                return Results.Ok("DEX file received successfully.");
+            }).RequireAuthorization();
+            #endregion
+        }
+    }
+}
